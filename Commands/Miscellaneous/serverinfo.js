4592ef62def4b5b33160cf3bb84f9c83cd3d76d0
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-07-10 20:27:42 -0500

    Update serverinfo.js

--- Commands/Miscellaneous/serverinfo.js
+++ Commands/Miscellaneous/serverinfo.js
@@ -1,5 +1,36 @@
 const { Command } = require('discord-akairo');
 const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+
+const filterLevels = {
+ DISABLED: 'Off',
+ MEMBERS_WITHOUT_ROLES: 'No Role',
+ ALL_MEMBERS: 'Everyone'
+};
+
+const verificationLevels = {
+ NONE: 'None',
+ LOW: 'Low',
+ MEDIUM: 'Medium',
+ HIGH: '(╯°□°）╯︵ ┻━┻',
+ VERY_HIGH: '┻━┻ ︵ヽ(`□´)ﾉ︵ ┻━┻'
+};
+
+const regions = {
+brazil: "Brazil",
+europe: "Europe",
+hongkong: "Hong Kong",
+india: "India",
+japan: "Japan",
+russia: "Russia",
+singapore: "Singapore",
+southafrica: "South Africa",
+sydeny: "Sydeny",
+'us-central': "US Central",
+'us-east': "US East",
+'us-west': "US West",
+'us-south': "US South"
+};
 
     class ServerInfoCommand extends Command {
         constructor() {
@@ -17,49 +48,62 @@ const { MessageEmbed } = require('discord.js');
         }
 
         exec(message) {
-            let region = {
-                "brazil": ":flag_br: `Brazil`",
-                "eu-central": ":flag_eu: `Central Europe`",
-                "singapore": ":flag_sg: `Singapore`",
-                "us-central": ":flag_us: `U.S. Central`",
-                "sydney": ":flag_au: `Sydney`",
-                "us-east": ":flag_us: `U.S. East`",
-                "us-south": ":flag_us: `U.S. South`",
-                "us-west": ":flag_us: `U.S. West`",
-                "eu-west": ":flag_eu: `Western Europe`",
-                "vip-us-east": ":flag_us: `VIP U.S. East`",
-                "london": ":flag_gb: `London`",
-                "amsterdam": ":flag_nl: `Amsterdam`",
-                "hongkong": ":flag_hk: `Hong Kong`",
-                "russia": ":flag_ru: `Russia`",
-                "southafrica": ":flag_za: `South Africa`"
-            };
+           
+              const roles = message.guild.roles.cache.sort((a,b) => b.position - a.position).map(role=> role.toString());
+  const members = message.guild.members.cache;
+  const channels = message.guild.channels.cache;
+  const emojis = message.guild.emojis.cache;
+  
+  
+    const embed = new MessageEmbed()
+    .setDescription(`**Guild information for _${message.guild.name}_**`)
+    .setColor(0xC76CF5)
+    .setThumbnail(message.guild.iconURL({ dynamic: true}))
+    .addField('General', [
+       `** Name:** ${message.guild.name}`,
+       `** ID:** ${message.guild.id}`,
+       `** Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
+       `** Region:** ${regions[message.guild.region]}`,
+       `** Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
+       `** Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
+       `** Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
+       `** Time Created:** ${moment(message.guild.createdTimestamp).format('LT')} ${moment(message.guild.createdTimestamp).format('Ll')} ${moment(message.guild.createdTimestamp).fromNow()}`,
+       '\u200b'
+    ])
+    .addField('Statistics', [
+       `** Roles:** ${roles.length}`,
+       `** Emojis:** ${emojis.size}`,
+       `** Regular Emojis:** ${emojis.filter(emoji => !emoji.animated).size}`,
+       `** Animated Emojis:** ${emojis.filter(emoji => emoji.animated).size}`,
+       `** Members Count:** ${message.guild.memberCount}`,
+       `** Human:** ${members.filter(member => !member.user.bot).size}`,
+       `** Bots:** ${members.filter(member => member.user.bot).size}`,
+       `** Text Channels:** ${channels.filter(channel => channel.type === 'text').size}`,
+       `** Voice Channel:** ${channels.filter(channel => channel.type === 'voice').size}`,
+       `** Boost Count:** ${message.guild.premiumSubscriptionCount || '0'}`,
+       
+     ], true)
+     .addField('Presence',[
+     `** Online:** ${members.filter(member => member.presence.status === 'online').size}`,
+     `** Idle:** ${members.filter(member => member.presence.status === 'idle').size}`,
+     `** Do not Disturb:** ${members.filter(member => member.presence.status === 'dnd').size}`,
+     `** Offline:** ${members.filter(member => member.presence.status === 'offline').size}`,
+     ], true)  
     
-            let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
-            let roles = message.guild.roles.filter(r => r.name !== '@everyone').sort((a, b) => b.position > a.position ? 1 : -1).first(15).map(r => r).join(', ');
-                if(message.guild.roles.size > 16) roles += `... 15/${message.guild.roles.size - 1}`
-            const embed = new MessageEmbed()
-                .setColor(this.client.colors['defaultColor'])
-                .setAuthor(`${message.guild.name} Information`, message.guild.iconURL())
-                .setThumbnail(message.guild.iconURL())
-                .addField('**Server ID**', `\`${message.guild.id}\``, true)
-                .addField('**Server Owner**', `\`${this.client.users.get(message.guild.ownerID).tag}\``, true)
-                .addField('**Region**', `${region[message.guild.region]}`, true)
-                .addField('**Total | Users | Bots**', `\`${message.guild.memberCount} | ${message.guild.members.filter(u => !u.user.bot).size} | ${message.guild.members.filter(u => u.user.bot).size}\``, true)
-                .addField('**Total | Text | Voice**', `\`${message.guild.channels.size} | ${message.guild.channels.filter(c => c.type === 'text').size} | ${message.guild.channels.filter(c => c.type === 'voice').size}\``, true)
-                .addField('**AFK Channel**', `\`${message.guild.afkChannel ? message.guild.afkChannel : 'None'}\``, true)
-                .addField('**Boosts**', `**Tier ${message.guild.premiumTier}**\n\`${message.guild.premiumSubscriptionCount}/30\``, true)
-                .addField('**Verification Level**', `\`${verifLevels[message.guild.verificationLevel]}\``, true)
-                .addField('**Created At**', `\`${new Date(message.guild.createdAt).toLocaleString('en-GB', { dateStyle: 'full'})}\``, true)
-                .addField('**Emojis**', `\`${message.guild.emojis.size}\``, true)
-                .addField('**Verified**', `\`${message.guild.verified ? '`Yes`' : '`No`'}\``, true)
-                .addField('**Partnered**', `\`${message.guild.verified ? '`Yes`' : '`No`'}\``, true)
-                .addField('**Static Emojis**', `${message.guild.emojis.first(15).filter(e => !e.animated).map(x => x).join(' ') || 'None'}`)
-                .addField('**Animated Emojis**', `${message.guild.emojis.first(15).filter(e => e.animated).map(x => x).join(' ')|| 'None'}`)
-                .addField('**Guild Features**', `\`\`\`${message.guild.features.join(', ') || 'No Features'}\`\`\``)
-                .addField(`**Roles [ ${message.guild.roles.size - 1} ]**`, `${roles}`)
-              message.channel.send(embed);
+    .addField(`Roles [${roles.length - 1}]`, roles.length < 10 ? roles.join(', ') : roles.length > 10 ? trimArray(roles) : 'None')
+    .setTimestamp();
+    message.channel.send(embed);
+            
         }
     }
+
+function trimArray(arr, maxLen = 10){
+if(arr.length > maxLen){
+const len = arr.length - maxLen;
+arr = arr.slice(0, maxLen);
+arr.push(`${len} more...`) ;
+}
+return arr;
+}
 
 module.exports = ServerInfoCommand;
