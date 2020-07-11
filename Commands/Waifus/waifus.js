3a5d26e4d83746733ba9028680db18c8779be5ed
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-07-11 17:26:25 -0500

    Update waifus.js

--- Commands/Waifus/waifus.js
+++ Commands/Waifus/waifus.js
@@ -44,7 +44,7 @@ registerFont('./Bebas.ttf', { family: 'Bebas' })
 	  ctx.font = applyText(canvas, `${waifuI.anime}!`);
 	  ctx.fillStyle = '#ffffff';
 	  ctx.textAlign = "center";	
-	  ctx.fillText(`${waifuI.anime}!`, 589, 1186);	
+	  ctx.fillText(`${waifuI.anime}!`, 800, 1186);	
 	  const attachment = new discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
 	  message.channel.send(`:round_pushpin:  |  Profile card of ${message.author.username}`, attachment);	
         }
