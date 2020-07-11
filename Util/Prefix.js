e8c1740f374b122961714609a392b98b75b3b646
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-07-10 21:34:29 -0500

    Create Prefix.js

---
+++ Util/Prefix.js
@@ -0,0 +1,24 @@
+const sqlite = require('sqlite');
+const sequelize = require('sequelize');
+
+const { AkairoClient, SQLiteProvider, SequelizeProvider } = require('discord-akairo');
+
+class CustomClient extends AkairoClient {
+    constructor() {
+        super({
+            /* Options here */
+        });
+
+        // With SQLite
+        this.settings = new SQLiteProvider(sqlite.open('path/to/db.sqlite'), 'table_name', {
+            idColumn: 'guild_id',
+            dataColumn: 'settings'
+        });
+
+        // Or, with sequelize
+        this.settings = new SequelizeProvider(/* Sequelize model here */, {
+            idColumn: 'guild_id',
+            dataColumn: 'settings'
+        });
+    }
+}
