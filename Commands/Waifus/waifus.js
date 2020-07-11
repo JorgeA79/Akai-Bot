74de9116662388abadb13ba8381d5856ba027df6
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-07-11 18:19:09 -0500

    Update waifus.js

--- Commands/Waifus/waifus.js
+++ Commands/Waifus/waifus.js
@@ -49,7 +49,7 @@ registerFont('./Bebas.ttf', { family: 'Bebas' })
 	  const attachment = new discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
   	  
 	  const embed = new discord.MessageEmbed()	
- 	  .attachFile(attachment)
+ 	  .attachFiles([attachment])
 	  .setImage('attachment://welcome-image.png');	 	
 	  message.channel.send(embed);	
         }
