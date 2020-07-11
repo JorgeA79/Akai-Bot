3cc24079fd28abb6b512244c3289767f07108e14
JorgeA79 <33409037+JorgeA79@users.noreply.github.com>
2020-07-10 20:52:32 -0500

    Create waifus.js

---
+++ Commands/Waifus/waifus.js
@@ -0,0 +1,46 @@
+const { Command } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
+const fs = require('fs');
+
+var contents = fs.readFileSync("./waifus.json");
+var jsonContent = JSON.parse(contents);
+
+    class PingCommand extends Command {
+        constructor() {
+            super('waifu', {
+                aliases: ['waifu', 'waifus'],
+                category: 'Anime',
+                description: {
+                    usage: 'waifu',
+                    examples: ['waifu Uraraka'],
+                    description: 'Waifus! Display's your waifus information'
+                },
+                cooldown: 2000,
+                ratelimit: 3
+            })
+        }
+
+        async exec(message) {
+            
+            const args = msg.content.slice(prefix.length).split(` `);
+		
+	          if (!args[1]) {
+	          return message.channel.send("You need to specify a Waifu");
+	          }
+            const waifus = {
+	          "URARAKA":jsonContent.waifus[0],
+          	"ATAGO":jsonContent.waifus[1],
+	          "SYLPHYN":jsonContent.waifus[2],	
+	           }
+	           const embed = new Discord.MessageEmbed()
+	              .setTitle(waifus[args[1].toUpperCase()].displayName)
+	              .setDescription(waifus[args[1].toUpperCase()].description)
+	              .addField("Anime/Game:", waifus[args[1].toUpperCase()].anime, true)
+	              .addField("Gender:", waifus[args[1].toUpperCase()].gender, true)
+	              .setThumbnail(waifus[args[1].toUpperCase()].image)
+	           return msg.channel.send(embed);
+            
+        }
+    }
+
+module.exports = WaifuCommand;
